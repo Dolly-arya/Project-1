be65395af4948999f9fe474e8d723e98d821ed72
Dolly-arya <dy9969336888@gmail.com>
2024-07-19 13:01:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var menuBtn = document.querySelector(".navbar-toggler"); //icon
+var menuCls = document.querySelector(".cls"); //close icon
+var menu = document.querySelector(".menu-items"); //menu
+//eventlistener
+menuBtn.addEventListener("click",function(){
+    menu.classList.toggle("active");
+})
+
+menuCls.addEventListener("click", function(){
+    menu.classList.toggle("active");
+})
